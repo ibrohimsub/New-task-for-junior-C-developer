2a1d988f3f284491dff1ea022bbb0c7cf099bec3
Ibrohim Subkhanqulov <ibragim.sub+sourcetree@gmail.com>
2023-06-30 18:38:24 +0500

    added "func that sends SMS" to costumer and comments

--- ProductService.cs
+++ ProductService.cs
@@ -2,9 +2,11 @@ public class ProductService
 {
     public decimal CalculateTotalPayment(string product, decimal amount, string phoneNumber, int installmentRange)
     {
+        // Проверка входных параметров, например, на валидность номера телефона, положительность суммы и т.д.
 
         decimal totalPayment = amount;
 
+        // Проверяем категорию продукта и применяем диапазон рассрочки с соответствующими процентами
         switch (product.ToLower())
         {
             case "смартфон":
@@ -26,8 +28,15 @@ public class ProductService
                 throw new ArgumentException("Неизвестная категория продукта.");
         }
 
+        // Отправляем СМС с деталями покупки на номер телефона клиента
         SendSMS(phoneNumber, $"Вы приобрели {product} на сумму {totalPayment} сомони в рассрочку на {installmentRange} месяцев.");
 
         return totalPayment;
+    }
+
+    private void SendSMS(string phoneNumber, string message)
+    {
+        // Код для отправки СМС на указанный номер телефона
+        // Например, можно использовать сторонний сервис для отправки СМС через API
     }
 }
